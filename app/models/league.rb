cc5e5f4552cf0cfe6f9c5b6bbe08a3defe90b9c1
Sean McNally <seanmcnally88@gmail.com>
2015-12-17 14:04:15 -0600

    Changed League and Sport model associations

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,6 +1,5 @@
 class League < ActiveRecord::Base
   belongs_to :sport
-  has_many :teams
   has_many :games
   has_many  :images
 end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,4 +1,5 @@
 class Sport < ActiveRecord::Base
   belongs_to :league
   belongs_to :team
+  has_many :images
 end
