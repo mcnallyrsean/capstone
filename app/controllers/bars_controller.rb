ecf1def7b5e900819a625e57a2702a39f7406579
Sean McNally <seanmcnally88@gmail.com>
2015-12-16 20:47:56 -0600

    Added Edit action for Bars

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -33,9 +33,28 @@ class BarsController < ApplicationController
   end
 
   def edit
+    @bar = Bar.find_by(id: params[:id])
+    render :edit
   end
 
   def update
+    @bar = Bar.find_by(id: params[:id])
+    if @bar.update(
+      id: params[:id],
+      name: params[:name],
+      location: params[:location],
+      description: params[:description],
+      hours_of_operation: params[:hours_of_operation],
+      status: params[:status],
+      promo: params[:promo],
+      facebook_url: params[:facebook_url],
+      twitter_handle: params[:twitter_handle],
+      number_of_tvs: params[:number_of_tvs]
+      )
+      redirect_to "/bars/#{@bar.id}"
+    else
+      render :edit
+    end
   end
 
   def destroy
