06ea3a2d3e7e3ebf0eb1d27f17bd2412139dbab2
Sean McNally <seanmcnally88@gmail.com>
2015-12-16 20:38:56 -0600

    Added create action for bars.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,15 +1,35 @@
 class BarsController < ApplicationController
 
   def index
+    @bars = Bars.all
   end
 
   def new
+    @bar = Bar.new
   end
 
   def create
+    @bar = Bar.new(
+      id: params[:id],
+      name: params[:name],
+      location: params[:location],
+      description: params[:description],
+      hours_of_operation: params[:hours_of_operation],
+      status: params[:status],
+      promo: params[:promo],
+      facebook_url: params[:facebook_url],
+      twitter_handle: params[:twitter_handle],
+      number_of_tvs: params[:number_of_tvs]
+      )
+    if @bar.save
+      redirect_to "/bars/#{@bar.id}"
+    else
+      render :new
+    end
   end
 
   def show
+    @bar = Bar.find_by(id: params[:id])
   end
 
   def edit
