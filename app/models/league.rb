0e50cc90f6a57d0ee3be7db812161885f5c5827e
Sean McNally <seanmcnally88@gmail.com>
2015-12-17 14:38:37 -0600

    Added attribute to League model, added has many through relationships to User model

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,4 +2,5 @@ class League < ActiveRecord::Base
   belongs_to :team
   has_many :games
   has_many  :images
+  has_one :sport
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217195126) do
+ActiveRecord::Schema.define(version: 20151217202220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20151217195126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "sport_id"
   end
 
   create_table "sports", force: :cascade do |t|
