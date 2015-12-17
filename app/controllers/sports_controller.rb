0439069f97c448c0291ffe3e77d91ed1a360fd43
Sean McNally <seanmcnally88@gmail.com>
2015-12-17 15:06:44 -0600

    Added index, show, new, and create actions for Sports controller

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,15 +1,27 @@
 class SportsController < ApplicationController
 
   def index
+    @sports = Sport.all
   end
 
   def new
+    @sports = Sport.new
+    render :new
   end
 
   def create
+    @sport = Sport.new(
+      name: params[:name]
+      )
+    if @sport.save
+      redirect_to "/sports/#{@sport.id}"
+    else
+      render :new
+    end
   end
 
   def show
+    @sport = Sport.find_by(id: params[:id])
   end
 
   def edit
