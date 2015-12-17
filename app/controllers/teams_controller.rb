9a2a88661599f4ffcec0a33195f83e601bad814b
Sean McNally <seanmcnally88@gmail.com>
2015-12-17 12:40:44 -0600

    Added index, show, new, and create actions in the teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,15 +1,29 @@
 class TeamsController < ApplicationController
 
   def index
+    @teams = Team.all
   end
 
   def new
+    @team = Team.new
+    render :new
   end
 
   def create
+    @team = Team.new(
+      name: params[:name],
+      league_id: params[:league_id],
+      sport_id: params[:sport_id],
+      )
+    if @team.save
+      redirect_to "/teams/#{team.id}"
+    else
+      render :new
+    end
   end
 
   def show
+    @team = Team.find_by(id: params[:id])
   end
 
   def edit
