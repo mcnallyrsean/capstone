87415d09e6d086e3c060b32d3d72768c155ec416
Sean McNally <seanmcnally88@gmail.com>
2015-12-17 12:14:15 -0600

    Added create action in the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,34 @@
 class GamesController < ApplicationController
 
   def index
+    @games = Game.all
   end
 
   def new
+    @game = Game.new
+    render :new
   end
 
   def create
+    @game = Game.new(
+      date: params[:date],
+      location: params[:location],
+      team_1_id: params[:team_1_id],
+      team_2_id: params[:team_2_id],
+      check_in_id: params[:check_in_id],
+      sport_id: params[:sport_id],
+      league_id: params[:league_id],
+      bar_id: params[:bar_id]
+      )
+    if @game.save
+      redirect_to "/games/#{@game.id}"
+    else
+      render :new
+    end
   end
 
   def show
+    @game = Game.find_by(id: params[:id])
   end
 
   def edit
