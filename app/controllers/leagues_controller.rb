37dd88ab75fe9eff573d888695dd29217acb113b
Sean McNally <seanmcnally88@gmail.com>
2015-12-16 21:16:22 -0600

    Added create action for League, add index and show views

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,15 +1,27 @@
 class LeaguesController < ApplicationController
 
   def index
+    @leagues = League.all
   end
 
   def new
+    @league = League.new
+    render :new
   end
 
   def create
+    @league = League.new(
+      name: params[:name],
+      )
+    if @league.save
+      redirect_to "/leagues/#{@league.id}"
+    else
+      render :new
+    end
   end
 
   def show
+    @league = League.find_by(id: params[:id])
   end
 
   def edit
