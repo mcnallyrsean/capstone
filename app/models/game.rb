e050504731438b77937c6b7d05521901fdc54ba9
Sean McNally <seanmcnally88@gmail.com>
2015-12-22 20:15:54 -0600

    Made model associations more granular

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ActiveRecord::Base
   has_many :teams, through: :matchups
-  belongs_to :sport
+  belongs_to :sport#, class_name: "Sport", foreign_key: "sport_id"
+  belongs_to :team_1, class_name: "Team", foreign_key: "team_1_id"
+  belongs_to :team_2, class_name: "Team", foreign_key: "team_2_id" 
   belongs_to :league
   belongs_to :bar
   has_many :images
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ActiveRecord::Base
   belongs_to :league
   has_one :sport, through: :league
   has_many :games, through: :matchups
-  has_many :images
+  has_many :images#, class_name: "Image", foreign_key: "team_id"
 end
